d1111f3870d45767b04eb4f0820065982bd99f5e
Michele Cattaneo <cattami@usi.ch>
2024-04-21 12:28:12 +0200

    Fixed discriminator

--- models/discriminator.py
+++ models/discriminator.py
@@ -10,7 +10,11 @@ class Discriminator(nn.Module):
         self.feature_dim = features_dim
 
         self.w = nn.Parameter(torch.Tensor(features_dim, features_dim))
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.w)
 
     def forward(self, Z, g):
-        return F.sigmoid(Z @ self.w @ g.mT)
+        return F.sigmoid(Z @ self.w @ g)
     
