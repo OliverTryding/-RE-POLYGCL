51e0c54b92eae09003660571d441b8836c40a936
Michele Cattaneo <cattami@usi.ch>
2024-04-21 12:27:19 +0200

    Fixed loss

--- loss.py
+++ loss.py
@@ -10,6 +10,6 @@ def contrastive_loss(Z_H,
                      D: Discriminator):
 
 
-    return torch.mean(torch.log(D(g, Z_L)) + torch.log(1 - D(g, Z_L_tilde)) + torch.log(D(g, Z_H)) + torch.log(1 - D(g, Z_H_tilde)))
+    return -torch.mean(torch.log(D(Z_L,g)) + torch.log(1 - D(Z_L_tilde,g)) + torch.log(D(Z_H,g)) + torch.log(1 - D(Z_H_tilde,g)))
 
     
