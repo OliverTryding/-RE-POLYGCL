66f228a4f92b27d3b50886b43c0ed2ac71209a7f
MicheleCattaneo <cattami@usi.ch>
2024-04-23 23:08:02 +0200

    small trainer update

--- trainer.py
+++ trainer.py
@@ -3,7 +3,8 @@ from torch_geometric.transforms import NormalizeFeatures
 from loss import contrastive_loss
 from models.PolyGCL_model import PolyGCL
 import torch
-
+from torch.utils.tensorboard import SummaryWriter
+import datetime
 
 
 
@@ -23,19 +24,45 @@ def train(model,optim,data):
     g = model.get_global_summary(pos_Z_L, pos_Z_H)
 
     loss = contrastive_loss(pos_Z_H, neg_Z_H, pos_Z_L, neg_Z_L, g, model.discriminator)
-    print(loss.item())
 
     loss.backward()
-    optimizer.step()
+    optim.step()
 
+    return loss.item()
 
 
 
 if __name__ == '__main__':
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
 
     dataset = Planetoid(root='data/Planetoid', name='Cora', transform=NormalizeFeatures())
-    model = PolyGCL(in_size=dataset.x.shape[-1], hidden_size=128, out_size=128, K=50, dropout_p=0.1)
+    model = PolyGCL(in_size=dataset.x.shape[-1], hidden_size=128, out_size=128, K=10, dropout_p=0.4)
+    model = model.to(device)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    for i in range(10):
-        train(model,optimizer,dataset[0])
+
+    
+    run_name = f'run_{datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")}'
+
+    writer = SummaryWriter(log_dir=f'runs/{run_name}')
+
+
+    data = dataset[0].to(device)
+
+    # TODO add early stopper
+    # TODO add validation and test mask -> compute loss only on train set
+    # TODO add evaluation step
+
+    for i in range(5000):
+        loss = train(model,optimizer,data)
+        print(loss)
+
+        print(model.encoder.convolution.gammas)
+        # print(model.encoder.convolution.temp_low)
+        # print(model.encoder.convolution.temp_high)
+
+        writer.add_scalar('Loss/train', loss, i)
+        writer.add_scalar('beta/train', model.beta, i)
+        writer.add_scalar('alpha/train', model.alpha, i)
